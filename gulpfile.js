2f0bf795e4fdaf08cc50c51da9a42f02fb8ec0ec
ekaterina <ekivanova72@gmail.com>
2023-03-01 23:32:24 +0500

    Добавляет разметку главной страницы

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ import del from 'del';
 import styles from './gulp/compileStyles.mjs';
 import { copy, copyImages, copySvg } from './gulp/copyAssets.mjs';
 import js from './gulp/compileScripts.mjs';
-import {optimizeSvg, sprite, createWebp, optimizePng, optimizeJpg} from './gulp/optimizeImages.mjs';
+import { optimizeSvg, sprite, createWebp, optimizePng, optimizeJpg } from './gulp/optimizeImages.mjs';
 
 const server = browserSync.create();
 const streamStyles = () => styles().pipe(server.stream());
@@ -43,4 +43,4 @@ const build = gulp.series(clean, copy, sprite, gulp.parallel(styles, js, optimiz
 const dev = gulp.series(clean, copy, sprite, gulp.parallel(styles, js, optimizePng, optimizeJpg, optimizeSvg), syncServer);
 const start = gulp.series(clean, copy, sprite, gulp.parallel(styles, js), syncServer);
 
-export { createWebp as webp, build, start, dev};
+export { createWebp as webp, build, start, dev };
